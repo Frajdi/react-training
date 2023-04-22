54d5c3cc3e9fb9f90f804876d37dd6773063a99b
AniKimza <ani.kimza@gmail.com>
2023-04-22 04:06:31 +0200

    Style submit with typography

--- src/MultiPageForm/Submit.jsx
+++ src/MultiPageForm/Submit.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react"
 import { DataContext } from "../App";
 import { useNavigate } from "react-router-dom";
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 
 const Submit = () => {
     const { formData } = useContext(DataContext)
@@ -13,29 +13,70 @@ const onHome = () => {
 }
 
     return <>
-        <h1>{formData.contact.name}'s information</h1>
-        <h2>Name:</h2>
-        <h3>{formData.contact.name}</h3>
-        <h2>Surname</h2>
-        <h3>{formData.contact.surName}</h3>
-        <h2>Email</h2>
-        <h3>{formData.contact.email}</h3>
-        <h2>Phone Number</h2>
-        <h3>{formData.contact.phoneNumber}</h3>
-        <h2>Country</h2>
-        <h3>{formData.adress.country}</h3>
-        <h2>City</h2>
-        <h3>{formData.adress.city}</h3>
-        <h2>Street Name</h2>
-        <h3>{formData.adress.streetName}</h3>
-        <h2>Company</h2>
-        <h3>{formData.work.company}</h3>
-        <h2>Job Position</h2>
-        <h3>{formData.work.jobPosition}</h3>
-        <h2>Salary</h2>
-        <h3>{formData.work.salary}</h3>
-        <h2>Currency</h2>
-        <h3>{formData.work.currency}</h3>
+        <Typography variant="h2" color={'#1976D2'}>
+        {formData.contact.name}'s information
+      </Typography>
+      <Typography variant="h4" color={'#1976D2'}>
+        Name:
+      </Typography>
+      <Typography variant="h5" color={'#001E3C'}>
+      {formData.contact.name}
+      </Typography>
+        <Typography variant="h4" color={'#1976D2'}>
+        Surname:
+      </Typography>
+      <Typography variant="h5" color={'#001E3C'}>
+      {formData.contact.surName}
+      </Typography>
+        <Typography variant="h4" color={'#1976D2'}>
+        Email:
+      </Typography>
+      <Typography variant="h5" color={'#001E3C'}>
+      {formData.contact.email}
+      </Typography>
+        <Typography variant="h4" color={'#1976D2'}>
+        Phone Number:
+      </Typography>
+      <Typography variant="h5" color={'#001E3C'}>
+      {formData.contact.phoneNumber}
+      </Typography>
+        <Typography variant="h4" color={'#1976D2'}>
+        Country:
+      </Typography>
+      <Typography variant="h5" color={'#001E3C'}>
+      {formData.adress.country}
+      </Typography>
+        <Typography variant="h4" color={'#1976D2'}>
+        City:
+      </Typography>
+      <Typography variant="h5" color={'#001E3C'}>
+      {formData.adress.city}
+      </Typography>
+        <Typography variant="h4" color={'#1976D2'}>
+        Street Name:
+      </Typography>
+      <Typography variant="h5" color={'#001E3C'}>
+      {formData.adress.streetName}
+      </Typography>
+        <Typography variant="h4" color={'#1976D2'}>
+        Company Name:
+      </Typography>
+      <Typography variant="h5" color={'#001E3C'}>
+      {formData.work.companyName}
+      </Typography>
+        <Typography variant="h4" color={'#1976D2'}>
+        Job Position:
+      </Typography>
+      <Typography variant="h5" color={'#001E3C'}>
+      {formData.work.jobPosition}
+      </Typography>
+        <Typography variant="h4" color={'#1976D2'}>
+        Salary:
+      </Typography>
+      <Typography variant="h5" color={'#001E3C'}>
+      {formData.work.salary}
+      {formData.work.currency}
+      </Typography>
         <Button onClick={onHome} variant="contained" sx={{height: 40}}>Back to Form</Button>
     </>
 }
