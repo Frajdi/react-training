f6cef828e3625c93407a0a58020600881d82dde8
AniKimza <ani.kimza@gmail.com>
2023-04-22 02:25:30 +0200

    Animation button with submit function

---
+++ src/MultiPageForm/Components/Switch.jsx
@@ -0,0 +1,74 @@
+import { React, useEffect } from "react";
+import { motion, useMotionValue, useMotionValueEvent, useTransform } from "framer-motion";
+import "./switch.css";
+
+export const Switch = (props) => {
+  const x = useMotionValue(0);
+  const xInput = [-100, 0, 100];
+  const background = useTransform(x, xInput, [
+    "#D32F2F",
+    "#1976D2",
+    "#001E3C"
+  ]);
+  const color = useTransform(x, xInput, [
+    "#D32F2F",
+    "#1976D2",
+    "#001E3C"
+  ]);
+  const tickPath = useTransform(x, [10, 100], [0, 1]);
+  const crossPathA = useTransform(x, [-10, -55], [0, 1]);
+  const crossPathB = useTransform(x, [-50, -100], [0, 1]);
+
+  useMotionValueEvent(x, "change", (latest) => {
+    if (latest >= 200) {
+      props.submit()
+    } else if (latest <= -200) {
+      
+    }
+  })
+
+  return (
+    <motion.div className="example-container" style={{ background }}>
+      <motion.div
+        className="box"
+        style={{ x }}
+        drag="x"
+        dragConstraints={{ left: 0, right: 0 }}
+      >
+        <svg className="progress-icon" viewBox="0 0 50 50">
+          <motion.path
+            fill="none"
+            strokeWidth="2"
+            stroke={color}
+            d="M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0"
+            style={{ translateX: 5, translateY: 5 }}
+          />
+          <motion.path
+            fill="none"
+            strokeWidth="2"
+            stroke={color}
+            d="M14,26 L 22,33 L 35,16"
+            strokeDasharray="0 1"
+            style={{ pathLength: tickPath }}
+          />
+          <motion.path
+            fill="none"
+            strokeWidth="2"
+            stroke={color}
+            d="M17,17 L33,33"
+            strokeDasharray="0 1"
+            style={{ pathLength: crossPathA }}
+          />
+          <motion.path
+            fill="none"
+            strokeWidth="2"
+            stroke={color}
+            d="M33,17 L17,33"
+            strokeDasharray="0 1"
+            style={{ pathLength: crossPathB }}
+          />
+        </svg>
+      </motion.div>
+    </motion.div>
+  );
+};
